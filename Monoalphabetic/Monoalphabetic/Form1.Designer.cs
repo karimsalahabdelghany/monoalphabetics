0b7541c2d5b590d82dd83f4c68527557746b1422
karim <karimabdelghany753@gmail.com>
2021-11-14 13:07:34 +0200

    Done

---
+++ Monoalphabetic/Monoalphabetic/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Monoalphabetic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.decText = new System.Windows.Forms.TextBox();
+            this.encText = new System.Windows.Forms.TextBox();
+            this.text = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(345, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 55);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(141, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Decryption";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(141, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Encryption";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(163, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Text";
+            // 
+            // decText
+            // 
+            this.decText.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.decText.Enabled = false;
+            this.decText.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decText.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.decText.Location = new System.Drawing.Point(247, 162);
+            this.decText.Name = "decText";
+            this.decText.Size = new System.Drawing.Size(224, 32);
+            this.decText.TabIndex = 5;
+            this.decText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // encText
+            // 
+            this.encText.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.encText.Enabled = false;
+            this.encText.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.encText.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.encText.Location = new System.Drawing.Point(247, 99);
+            this.encText.Name = "encText";
+            this.encText.Size = new System.Drawing.Size(224, 32);
+            this.encText.TabIndex = 6;
+            this.encText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // text
+            // 
+            this.text.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.text.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text.Location = new System.Drawing.Point(247, 32);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(224, 32);
+            this.text.TabIndex = 7;
+            this.text.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(214, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 55);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Encrypt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(637, 351);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.decText);
+            this.Controls.Add(this.encText);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.button3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Monoalphabetic";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox decText;
+        private System.Windows.Forms.TextBox encText;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Monoalphabetic/Monoalphabetic/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monoalphabetic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String text = this.text.Text;
+            char[] alphabetics = {'A', 'B', 'C', 'D', 'E', 'F'
+                    , 'G', 'H', 'I', 'J', 'K', 'L'
+                    , 'M', 'N', 'O', 'P', 'Q', 'R'
+                    , 'S', 'T', 'U', 'V', 'W', 'X'
+                    , 'Y', 'Z'};
+
+            //String[] keys = {"M", "N", "O", "P", "Q", "R"
+            //        , "G", "H", "I", "J", "K", "L"
+            //        , "S", "T", "U", "V", "W", "X"
+            //        ,"A", "B", "C", "D", "Y", "Z"
+            //        , "E", "F"};
+            String key = "MNOPGQRKJHISLTUVWXABCDYZEF";
+            String encryptedText = "";
+            //String k = String.Empty;
+
+            //Dictionary<String, String> letters = new Dictionary<String, String> { };
+
+            //for(int i=0; i < alphabetics.Length; i++)
+            //{
+            //    letters.Add(alphabetics[i], keys[i]);
+            //}
+
+            //for(int j = 0; j < text.Length; j++)
+            //{
+            //    foreach(KeyValuePair<String, String> letter in letters)
+            //    {
+            //        if (text[j] == letter[])
+            //        {
+
+            //        }
+            //    }
+            //}
+
+            //text = text.ToUpper();
+
+            foreach(char letter in text)
+            {
+                if(char.IsDigit(letter))
+                {
+                    MessageBox.Show("The text must be only alphabetics");
+                    this.text.Focus();
+                    this.text.Text = "Hello";
+                    break;
+                }
+                bool checkLower = char.IsLower(letter);
+                //MessageBox.Show(checkLower.ToString() + "\n" + letter);
+                for (int i = 0; i < alphabetics.Length; i++)
+                {
+                    if (char.ToUpper(letter) == alphabetics[i])
+                    {
+                        if(checkLower)
+                        {
+                            encryptedText += char.ToLower(key[i]);
+                            //MessageBox.Show(key[i].ToString().ToLower());
+                        }
+                        else
+                        {
+                            encryptedText += key[i];
+                        }
+                        break;
+                    }
+                    continue;
+                }
+            }
+            //MessageBox.Show(encryptedText);
+            this.encText.Text = encryptedText;
+            this.encText.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String text = this.text.Text;
+            char[] alphabetics = {'A', 'B', 'C', 'D', 'E', 'F'
+                    , 'G', 'H', 'I', 'J', 'K', 'L'
+                    , 'M', 'N', 'O', 'P', 'Q', 'R'
+                    , 'S', 'T', 'U', 'V', 'W', 'X'
+                    , 'Y', 'Z'};
+
+            String key = "MNOPGQRKJHISLTUVWXABCDYZEF";
+            String decryptedText = "";
+
+            foreach (char letter in text)
+            {
+                if (char.IsDigit(letter))
+                {
+                    MessageBox.Show("The text must be only alphabetics");
+                    this.text.Focus();
+                    break;
+                }
+                bool checkLower = char.IsLower(letter);
+                for (int i = 0; i < key.Length; i++)
+                {
+                    if (char.ToUpper(letter) == key[i])
+                    {
+                        if (checkLower)
+                        {
+                            decryptedText += alphabetics[i].ToString().ToLower();
+                        }
+                        else
+                        {
+                            decryptedText += alphabetics[i];
+                        }
+                        break;
+                    }
+                    continue;
+                }
+            }
+            this.decText.Text = decryptedText;
+            this.decText.Focus();
+        }
+    }
+}
